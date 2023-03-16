a0f34b938b3d3fd40d02b1545e8ee2a109136543
efaoro <efaoro>
2023-03-16 19:13:57 -0300

    criação do componente

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
+import { render } from '@testing-library/react';
+import { useEffect, useState } from 'react';
 import './App.css';
+import road_races from './assets/data/tracks.json'
+import header from './assets/imgs/header.jpg'
+
 
 function App() {
+  const [random, setRandom] = useState(0);
+  const [pressed, setPressed] = useState(false);
+  
+  function getTrack() {
+    setPressed(true)
+    setRandom((Math.random() * (43 - 1) + 1).toFixed())
+  };
+
+  useEffect(() => {
+    console.log(road_races.road_races[random].id)
+  }, [random]);
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="App-header" style={{ backgroundImage: `url(${header})`, backgroundPosition: 'center', backgroundSize: '300vh'}} >
       </header>
+      <div className='body-div'>
+        <button className='button button4' onClick={() => getTrack()}> Randomizar Pista </button>
+        <div className='track-div'>
+            {pressed
+              ? <>
+                  <p>{road_races.road_races[random].name}</p>
+                  <img src={road_races.road_races[random].img} style={{ width: '100vh'}} />
+                </>
+              : <></>
+            }
+          </div>
+      </div>
     </div>
   );
 }
-
 export default App;
